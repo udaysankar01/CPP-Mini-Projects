14afd21744698927aacabf6a9b024f81539e50ca
Uday Sankar <udaysankar.ambadi@gmail.com>
2024-06-08 19:30:30 -0400

    tetris game loop and basic mechanics setup

---
+++ Tetris/grid.cpp
@@ -0,0 +1,59 @@
+#include "grid.h"
+#include "colors.h"
+#include <iostream>
+#include <raylib.h>
+#include <vector>
+
+Grid::Grid()
+{
+	numRows = 20;
+	numCols = 10;
+	cellSize = 30;
+	Initialize();
+	colors = GetCellColors();
+}
+
+void Grid::Initialize()
+{
+	for (int row = 0; row < numRows; row++) 
+	{
+		for (int column = 0; column < numCols; column++) 
+		{
+			grid[row][column] = 0;
+		}
+	}
+}
+
+void Grid::Print()
+{	
+	std::cout << std::endl;
+	for (int row = 0; row < numRows; row++)
+	{
+		for (int column = 0; column < numCols; column++)
+		{
+			std::cout << grid[row][column] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void Grid::Draw()
+{
+	for (int row = 0; row < numRows; row++)
+	{
+		for (int column = 0; column < numCols; column++)
+		{
+			int cellValue = grid[row][column];
+			DrawRectangle(column*cellSize+1, row*cellSize+1, cellSize-1, cellSize-1, colors[cellValue]);
+		}
+	}
+}
+
+bool Grid::IsCellOutside(int row, int column)
+{
+	if (row >= 0 && row <= numRows || column >= 0 && column <= numCols)
+	{
+		return false;
+	}
+	return true;
+}
---
+++ Tetris/grid.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <raylib.h>
+
+class Grid
+{
+public:
+	Grid();
+	void Initialize();
+	void Print();
+	void Draw();
+	bool IsCellOutside(int row, int column);
+	int grid[20][10];
+private:
+	int numRows;
+	int numCols;
+	int cellSize;
+	std::vector<Color> colors;
+};
