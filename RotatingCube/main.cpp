3099d568c194c746db6695b8a1cc875376c2136c
Uday Sankar <udaysankar.ambadi@gmail.com>
2024-06-29 21:51:01 -0400

    rotCube plane rotating

--- RotatingCube/main.cpp
+++ RotatingCube/main.cpp
@@ -1,82 +1,77 @@
+#include <cstdio>
+#include <cstring>
 #include <iostream>
 #include <math.h>
-#include <cstring>
+#include <unistd.h>
 
-int A = 30, B=30, C=30;
-float x,y,z;
+float A = 0, B = 0, C = 0;
+float x, y, z;
 float ooz;
 int xp, yp;
 int idx;
 
-float cubeWidth=10;
-int  width=160, height=44;
-float zBuffer[160*44];
-char buffer[160*44];
+float cubeWidth = 10;
+int width = 160, height = 44;
+float zBuffer[160 * 44];
+char buffer[160 * 44];
 int backgroundASCIICode = ' ';
-float incrementSpeed=0.6;
-float distFromCam = 60;
-float K1=40;
+float incrementSpeed = 0.6;
+float distFromCam = 150;
+float K1 = 50;
 
 float calculateX(int i, int j, int k)
 {
-	return j*sin(A)*sin(B)*cos(C) - 
-		k*cos(A)*sin(B)*cos(C) + 
-		j*cos(A)*sin(C) + 
-		k*sin(A)*sin(C) + 
-		i*cos(B)*cos(C);
+    return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) + j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C);
 }
 
 float calculateY(int i, int j, int k)
 {
-	return j*cos(A)*cos(C) + 
-		k*sin(A)*cos(C) - 
-		j*sin(A)*sin(B)*sin(C) +
-		k*cos(A)*sin(B)*sin(C) - 
-		i*cos(B)*sin(C);
+    return j * cos(A) * cos(C) + k * sin(A) * cos(C) - j * sin(A) * sin(B) * sin(C) + k * cos(A) * sin(B) * sin(C) - i * cos(B) * sin(C);
 }
 
 float calculateZ(int i, int j, int k)
 {
-	return k*cos(A)*cos(B) - j*sin(A)*cos(B) + i*sin(B);
+    return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
 }
 
 void calculateForSurface(float cubeX, float cubeY, float cubeZ, int ch)
 {
-	x = calculateX(cubeX, cubeY, cubeZ);
-	y = calculateY(cubeX, cubeY, cubeZ);	
-	z = calculateZ(cubeX, cubeY, cubeZ) + distFromCam;
-	
-	ooz = 1/z;
-	xp = (int)(width/2 + K1*ooz*x*2);
-       	yp = (int)(height/2 + K1*ooz*y*2);	
-	
-	idx = xp + yp*width;
-	if (idx>=0 && idx<width*height)
-	{
-		if (ooz > zBuffer[idx])
-		{
-			zBuffer[idx] = ooz;
-			buffer[idx] = ch;
-		}
-	}	
+    x = calculateX(cubeX, cubeY, cubeZ);
+    y = calculateY(cubeX, cubeY, cubeZ);
+    z = calculateZ(cubeX, cubeY, cubeZ) + distFromCam;
+
+    ooz = 1 / z;
+    xp = (int)(width / 2 + K1 * ooz * x * 2);
+    yp = (int)(height / 2 + K1 * ooz * y * 2);
+
+    idx = xp + yp * width;
+    if (idx >= 0 && idx < width * height) {
+        if (ooz > zBuffer[idx]) {
+            zBuffer[idx] = ooz;
+            buffer[idx] = ch;
+        }
+    }
 }
-	
+
 int main()
 {
-	std::cout << "\x1b[2J";
-	while(1)
-	{
-		memset(buffer, backgroundASCIICode, width*height);
-		memset(zBuffer, 0, width*height*4); 
-		for(float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed)
-		{
-			for(float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed)
-			{
-				calculateForSurface(cubeX, cubeY, -cubeWidth, '#');
-		
-			}
-		}
-		std::cout << "\x1b[H";
-	}
-	return 0;
+    std::cout << "\x1b[2J";
+    while (1) {
+        memset(buffer, backgroundASCIICode, width * height);
+        memset(zBuffer, 0, width * height * 4);
+        for (float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed) {
+            for (float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed) {
+                calculateForSurface(cubeX, cubeY, -cubeWidth, '#');
+            }
+        }
+        std::cout << "\x1b[H";
+        for (int k = 0; k < width * height; k++) {
+            putchar(k % width ? buffer[k] : 10);
+        }
+
+        A += 0.005;
+        B += 0.005;
+        usleep(2000);
+    }
+    return 0;
 }
